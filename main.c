2070e1df9e0b51908386414264367bbb4d1f3f95
Nico Köpf <koepf.n@gmail.com>
2020-06-03 16:49:43 +0200

    updatetd gitignore to ignore atmel studio files and created project

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * MeBike.c
+ *
+ * Created: 03.06.2020 16:36:07
+ * Author : nicok
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
