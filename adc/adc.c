89f7924508a3af7f797f25464967a4d4cfc27e46
Frederic187 <henn.frederic@web.de>
2020-07-02 16:52:58 +0200

    removed unused libraries/variables and renamed variables

--- adc/adc.c
+++ adc/adc.c
@@ -18,16 +18,3 @@ void ADC_init()
     ADCSRA |= (1 << ADIE);    // Enable Interrupts
     ADCSRA |= (1 << ADSC);    // Start the ADC conversion
 }
-
-void interrupt_init()
-{
-    // Enable timer 2 to count the millis
-    // Set the Timer Mode to CTC
-    TCCR2A |= (1 << WGM21);
-    // Count every milli second ((16*10^6 / 256) * 0,001) -1
-    OCR2A = 61;
-    TIMSK2 |= (1 << OCIE2A);    //Set the ISR COMPA vect
-    TCCR2B |= (1 << CS22) | (1 << CS21); // set prescaler to 256 and start the timer
-
-}
-
